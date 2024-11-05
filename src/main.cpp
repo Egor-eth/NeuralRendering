79ea7705070f4e34d7ef0f9f2eef0ae89f23ec0b
RomanRodionov <rodionovv22@gmail.com>
2024-11-05 15:46:16 +0300

    Edit

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,6 @@ int main()
 {
     N_BVH nbvh;
     nbvh.SetAccelStruct(std::make_shared<BVH2CommonRT>());
-    nbvh.LoadSceneGLTF(std::string(std::filesystem::current_path()) + "/resources/scenes/01_Bunny/01_Bunny.gltf");
+    nbvh.LoadScene((std::string(std::filesystem::current_path()) + "/resources/scenes/01_Bunny/01_Bunny.gltf").c_str());
     std::cout << "Current path is " << std::filesystem::current_path() << std::endl;
 }
--- src/nbvh_host.cpp
+++ src/nbvh_host.cpp
@@ -1,4 +1,7 @@
 #include "nbvh.h"
+#include "hydraxml.h"
+#include "loader_utils/gltf_loader.h"
+#include "Timer.h"
 
 using LiteMath::DEG_TO_RAD;
 
@@ -11,12 +14,109 @@ using LiteMath::perspectiveMatrix;
 using LiteMath::lookAt;
 using LiteMath::inverse4x4;
 
-N_BVH::N_BVH()
-{
-    m_width = m_height = 500;
+N_BVH::N_BVH() 
+{ 
+  m_pAccelStruct = nullptr;
 }
 
-N_BVH::~N_BVH(){}
+void N_BVH::SetViewport(int a_xStart, int a_yStart, int a_width, int a_height)
+{
+  m_width  = a_width;
+  m_height = a_height;
+  m_packedXY.resize(m_width*m_height);
+}
+
+#if defined(__ANDROID__)
+bool N_BVH::LoadScene(const char* a_scenePath, AAssetManager* assetManager)
+#else
+bool N_BVH::LoadScene(const char* a_scenePath)
+#endif
+{
+  m_pAccelStruct->ClearGeom();
+
+  const std::string& path = a_scenePath;
+
+  auto isHydraScene = gltf_loader::ends_with(path, ".xml");
+
+  if(isHydraScene)
+  {
+#if defined(__ANDROID__)
+    return LoadSceneHydra(path, assetManager);
+#else
+    return LoadSceneHydra(path);
+#endif
+  }
+  else
+  {
+#if defined(__ANDROID__)
+    return LoadSceneGLTF(path, assetManager);
+#else
+    return LoadSceneGLTF(path);
+#endif
+  }
+
+  return false;
+}
+
+#ifdef __ANDROID__
+bool N_BVH::LoadSceneHydra(const std::string& a_path, AAssetManager* assetManager)
+#else
+bool N_BVH::LoadSceneHydra(const std::string& a_path)
+#endif
+{
+  hydra_xml::HydraScene scene;
+  if(
+#if defined(__ANDROID__)
+scene.LoadState(assetManager, a_path) < 0
+#else
+scene.LoadState(a_path) < 0
+#endif
+          )
+    return false;
+
+  for(auto cam : scene.Cameras())
+  {
+    float aspect   = float(m_width) / float(m_height);
+    auto proj      = perspectiveMatrix(cam.fov, aspect, cam.nearPlane, cam.farPlane);
+    auto worldView = lookAt(float3(cam.pos), float3(cam.lookAt), float3(cam.up));
+
+    m_projInv      = inverse4x4(proj);
+    m_worldViewInv = inverse4x4(worldView);
+
+    break; // take first cam
+  }
+
+  std::vector<uint64_t> trisPerObject;
+  trisPerObject.reserve(1000);
+  m_totalTris = 0;
+  m_pAccelStruct->ClearGeom();
+  for(auto meshPath : scene.MeshFiles())
+  {
+    std::cout << "[LoadScene]: mesh = " << meshPath.c_str() << std::endl;
+#if defined(__ANDROID__)
+    auto currMesh = cmes4h::LoadMeshFromVSGF(assetManager, meshPath.c_str());
+#else
+    auto currMesh = cmesh4::LoadMeshFromVSGF(meshPath.c_str());
+#endif
+    auto geomId   = m_pAccelStruct->AddGeom_Triangles3f((const float*)currMesh.vPos4f.data(), currMesh.vPos4f.size(),
+                                                        currMesh.indices.data(), currMesh.indices.size(), BUILD_HIGH, sizeof(float)*4);
+    (void)geomId; // silence "unused variable" compiler warnings
+    m_totalTris += currMesh.indices.size()/3;
+    trisPerObject.push_back(currMesh.indices.size()/3);
+  }
+  
+  m_totalTrisVisiable = 0;
+  m_pAccelStruct->ClearScene();
+  for(auto inst : scene.InstancesGeom())
+  {
+    m_pAccelStruct->AddInstance(inst.geomId, inst.matrix);
+    m_totalTrisVisiable += trisPerObject[inst.geomId];
+  }
+  m_pAccelStruct->CommitScene();
+
+  return true;
+}
+
 
 struct DataRefs
 {
@@ -67,8 +167,7 @@ static void LoadGLTFNodesRecursive(const tinygltf::Model &a_model, const tinyglt
         auto meshId = a_refs.m_pAccelStruct->AddGeom_Triangles3f((const float*)simpleMesh.vPos4f.data(), simpleMesh.vPos4f.size(),
                                                                 simpleMesh.indices.data(), simpleMesh.indices.size(),
                                                                 BUILD_HIGH, sizeof(float)*4);
-        a_refs.m_loadedMeshesToMeshId[a_node.mesh] = {meshId, simpleMesh.bbox}; 
-        //bbox?!
+        a_refs.m_loadedMeshesToMeshId[a_node.mesh] = {meshId, simpleMesh.bbox};
 
         std::cout << "Loading mesh # " << meshId << std::endl;
 
@@ -89,18 +188,26 @@ static void LoadGLTFNodesRecursive(const tinygltf::Model &a_model, const tinyglt
   }
 }
 
-bool N_BVH::LoadSceneGLTF(std::string a_path)
+
+#ifdef __ANDROID__
+bool N_BVH::LoadSceneGLTF(const std::string& a_path, AAssetManager* assetManager)
+#else
+bool N_BVH::LoadSceneGLTF(const std::string& a_path)
+#endif
 {
   tinygltf::Model gltfModel;
   tinygltf::TinyGLTF gltfContext;
   std::string error, warning;
 
+#ifdef __ANDROID__
+  tinygltf::asset_manager = assetManager;
+#endif
+
   bool loaded = gltfContext.LoadASCIIFromFile(&gltfModel, &error, &warning, a_path);
 
   if(!loaded)
   {
-    std::cerr << "Cannot load glTF scene from: " << a_path << std::endl;
-    std::cerr << error << a_path << std::endl;
+    std::cerr << "Cannot load glTF scene from: " << a_path << std::endl;;
     return false;
   }
 
@@ -124,6 +231,7 @@ bool N_BVH::LoadSceneGLTF(std::string a_path)
       std::cerr << "Orthographic camera not supported!" << std::endl;
     }
   }
+
 
   // load and instance geometry
   std::vector<uint64_t> trisPerObject;
@@ -161,3 +269,136 @@ bool N_BVH::LoadSceneGLTF(std::string a_path)
 
   return true;
 }
+
+
+#if defined(__ANDROID__)
+bool N_BVH::LoadSingleMesh(const char* a_meshPath, const float* transform4x4ColMajor, AAssetManager * assetManager)
+#else
+bool N_BVH::LoadSingleMesh(const char* a_meshPath, const float* transform4x4ColMajor)
+#endif
+{
+  m_pAccelStruct->ClearGeom();
+
+  std::cout << "[LoadScene]: mesh = " << a_meshPath << std::endl;
+#if defined(__ANDROID__)
+  auto currMesh = cmesh4::LoadMeshFromVSGF(assetManager, a_meshPath);
+#else
+  auto currMesh = cmesh4::LoadMeshFromVSGF(a_meshPath);
+#endif
+  if(currMesh.TrianglesNum() == 0)
+  {
+    std::cout << "[LoadScene]: can't load mesh '" << a_meshPath << "'" << std::endl;
+    return false;
+  }
+
+  auto geomId   = m_pAccelStruct->AddGeom_Triangles3f((const float*)currMesh.vPos4f.data(), currMesh.vPos4f.size(),
+                                                      currMesh.indices.data(), currMesh.indices.size(), BUILD_HIGH, sizeof(float)*4);
+  float4x4 mtransform;
+  if(transform4x4ColMajor != nullptr)
+  {
+    mtransform.set_col(0, float4(transform4x4ColMajor+0));
+    mtransform.set_col(1, float4(transform4x4ColMajor+4));
+    mtransform.set_col(2, float4(transform4x4ColMajor+8));
+    mtransform.set_col(3, float4(transform4x4ColMajor+12));
+  }
+
+  m_pAccelStruct->ClearScene();
+  m_pAccelStruct->AddInstance(geomId, mtransform);
+  m_pAccelStruct->CommitScene();
+  
+  float3 camPos  = float3(0,0,5);
+  float aspect   = float(m_width) / float(m_height);
+  auto proj      = perspectiveMatrix(45.0f, aspect, 0.01f, 100.0f);
+  auto worldView = lookAt(camPos, float3(0,0,0), float3(0,1,0));
+  
+  m_projInv      = inverse4x4(proj);
+  m_worldViewInv = inverse4x4(worldView);
+
+  return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+void N_BVH::Render(uint32_t* a_outColor, uint32_t a_width, uint32_t a_height, const char* a_what, int a_passNum)
+{
+  CastRaySingleBlock(a_width*a_height, a_outColor, a_passNum);
+}
+
+void N_BVH::CastRaySingleBlock(uint32_t tidX, uint32_t * out_color, uint32_t a_numPasses)
+{
+  profiling::Timer timer;
+  
+  #ifndef _DEBUG
+  #ifndef ENABLE_METRICS
+  #pragma omp parallel for default(shared)
+  #endif
+  #endif
+  for(int i=0;i<tidX;i++)
+    CastRaySingle(i, out_color);
+
+  //printf("CastSingleRayBlock...\n");
+  //
+  //auto a_width  = tidX;
+  //auto a_height = tidY;
+  //m_avgLCV = 0.0;
+  //
+  //#ifndef _DEBUG
+  //#ifndef ENABLE_METRICS
+  //#pragma omp parallel for collapse (2)
+  //#endif
+  //#endif
+  //for (int j = 0; j < int(a_height); j+=BSIZE)
+  //  for (int i = 0; i < int(a_width); i+=BSIZE)
+  //    CastRayPacket(i, j, out_color);
+  //
+  //m_avgLCV /= double( (a_height*a_width)/(BSIZE*BSIZE) );
+  //
+  timeDataByName["CastRaySingleBlock"] = timer.getElapsedTime().asMilliseconds();
+  //printf("CastRaySingleBlock %8.3f sec\n", timer.getElapsedTime().asSeconds());
+}
+
+const char* N_BVH::Name() const
+{
+  std::stringstream strout;
+  strout << "N_BVH(" << m_pAccelStruct->Name() << ")";
+  m_tempName = strout.str();
+  return m_tempName.c_str();
+}
+
+CustomMetrics N_BVH::GetMetrics() const 
+{
+  auto traceMetrics = m_pAccelStruct->GetStats();
+  CustomMetrics res = {};
+  for (int i = 0; i < TREELET_ARR_SIZE; i++) {
+    res.ljc_data[i] = traceMetrics.avgLJC[i];
+    res.cmc_data[i] = traceMetrics.avgCMC[i];
+    res.wss_data[i] = float(traceMetrics.avgWSS[i])/1000.0f;
+  }
+  res.common_data[0] = traceMetrics.avgNC;
+  res.common_data[1] = traceMetrics.avgLC;
+  res.common_data[2] = float(m_avgLCV);
+  res.common_data[3] = traceMetrics.avgTC;
+  res.common_data[4] = traceMetrics.avgBLB;
+  res.common_data[5] = traceMetrics.avgSOC;
+  res.common_data[6] = traceMetrics.avgSBL;
+  res.common_data[7] = 0.0f; // traceMetrics.avgTS;
+  res.size_data[0] = traceMetrics.bvhTotalSize;
+  res.size_data[1] = traceMetrics.geomTotalSize;
+  res.prims_count[0] = m_totalTrisVisiable;
+  res.prims_count[1] = m_totalTris;
+  return res;
+}
+
+
+void N_BVH::GetExecutionTime(const char* a_funcName, float a_out[4])
+{
+  auto p = timeDataByName.find(a_funcName);
+  if(p == timeDataByName.end())
+    return;
+  a_out[0] = p->second;
+}
+
+
+IRenderer* MakeEyeRayShooterRenderer(const char* a_name) { return new N_BVH; }
